055a2bf6be1e0ff5554ab8af762ef04361bcaabf
mrgenco <mrgenco@gmail.com>
2024-12-30 22:44:40 +0300

    Initial Commit

---
+++ HabitsChain/HabitsChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsChainApp.swift
+//  HabitsChain
+//
+//  Created by Mehmet Rasid Gencosmanoglu on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitsChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitsChainTests/HabitsChainTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsChainTests.swift
+//  HabitsChainTests
+//
+//  Created by Mehmet Rasid Gencosmanoglu on 30.12.2024.
+//
+
+import Testing
+@testable import HabitsChain
+
+struct HabitsChainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
